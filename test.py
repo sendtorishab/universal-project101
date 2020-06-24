f0067a51fc371e29a1206fa217648a6b43d5ba39
Rishab Singh <48208445+sendtorishab@users.noreply.github.com>
2020-06-24 15:36:06 +0530

    7th attempt

--- test.py
+++ test.py
@@ -6,4 +6,6 @@ from scipy import integrate
 i=scipy.integrate.quad(lambda x:2*x,0,3)
 print(i)
 i2=scipy.integrate.quad(lambda x:3*x,0,3)
-print(i)
+print(i2)
+i3=scipy.integrate.quad(lambda x:4*x,0,3)
+print(i3)
